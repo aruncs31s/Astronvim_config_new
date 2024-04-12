4b1bc904935d19d9d227d809056738d6256bbd6e
aruncs31s <aruncs31ss@gmail.com>
2024-04-12 22:29:50 +0530

    new file:   .luarc.json 	modified:   lazy-lock.json 	modified:   lua/plugins/astrolsp.lua modified:   lua/plugins/mappings.lua 	modified:   lua/plugins/user.lua

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -4,3 +4,8 @@ vim.api.nvim_set_keymap("n", "<A-w>", ":Telescope live_grep<CR>", { noremap = tr
 vim.api.nvim_set_keymap("n", "<A-f>", ":Telescope current_buffer_fuzzy_find<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<A-o>", ":Telescope oldfiles<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<A-d>", ":Telescope find_files<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-z>", ":Telescope zoxide list<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-h>", ":bp<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-l>", ":bn<CR>", { noremap = true, silent = true })
+-- TODO: Add git Mappings
+-- TODO; Add zoxide
