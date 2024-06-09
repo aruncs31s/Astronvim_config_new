ab4c9899eacc6c2d4eacd2eac92274d57ce8e549
aruncs31s <aruncs31ss@gmail.com>
2024-06-10 03:21:23 +0530

    update

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -8,8 +8,19 @@ return {
   vim.api.nvim_set_keymap("n", "<A-z>", ":Telescope zoxide list<CR>", { noremap = true, silent = true }),
   vim.api.nvim_set_keymap("n", "<S-h>", ":bp<CR>", { noremap = true, silent = true }),
   vim.api.nvim_set_keymap("n", "<S-l>", ":bn<CR>", { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap("n", "<A-h>", ":bp<CR>", { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap("n", "<A-l>", ":bn<CR>", { noremap = true, silent = true }),
   vim.api.nvim_set_keymap("n", "<A-t>", ":Telescope todo-comments<CR>", { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap("n", "<A-T>", ":ToggleTerm direction=float<CR>", { noremap = true, silent = true }),
+  --- Visual
+  vim.api.nvim_set_keymap("v", "<A-h>", ":bp<CR>", { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap("v", "<A-l>", ":bn<CR>", { noremap = true, silent = true }),
 
   -- TODO: Add git Mappings,
   -- TODO: Add zoxide,
+
+  -- Map Alt+> to Ctrl-i
+  -- vim.api.nvim_set_keymap("n", "gi", "<C-i>", { noremap = true, silent = true }),
+
+  -- Map Alt+< to Ctrl-o
 }
